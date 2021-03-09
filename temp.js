be9e9cd6989fff2c4634549f283276ff74daf874
Ahmed Shaalan <68456008+Ahmed-983@users.noreply.github.com>
2021-03-08 18:32:39 -0800

    Add files via upload

---
+++ temp.js
@@ -0,0 +1,11 @@
+function convert(degree) {
+    var x;
+    if (degree == "C") {
+        x = document.getElementById("celsius").value * 9 / 5 + 32;
+        document.getElementById("fahrenheit").value = Math.round(x);
+
+    } else {
+        x = (document.getElementById("fahrenheit").value - 32) * 5 / 9;
+        document.getElementById("celsius").value = Math.round(x);
+    }
+}
